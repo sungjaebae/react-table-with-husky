7d0499178b260dcc698d54a4de890c9010d15a4f
sungjae bae <hsm0156@gmail.com>
2023-04-12 18:32:42 +0900

    feat: axios 버전 추가

--- src/components/react-query/pages/api/posts.js
+++ src/components/react-query/pages/api/posts.js
@@ -1,38 +1,65 @@
+import axios from 'axios'
+
 export async function fetchPosts() {
-  const response = await fetch('http://localhost:3000/posts')
-  return response.json()
+  const response = await axios('http://localhost:3000/posts')
+  return response.data
 }
 
 export async function fetchPost(id) {
-  const response = await fetch(`http://localhost:3000/posts/${id}`)
-  return response.json()
+  const response = await axios(`http://localhost:3000/posts/${id}`)
+  return response.data
 }
 
 export async function createPost(newPost) {
-  const response = await fetch(`http://localhost:3000/posts`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newPost),
-  })
-  return response.json()
+  const response = await axios.post(`http://localhost:3000/posts`, newPost)
+  return response.data
 }
 
 export async function updatePost(updatedPost) {
-  const response = await fetch(`http://localhost:3000/posts/${updatedPost.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedPost),
-  })
-  return response.json()
+  const response = await axios.put(`http://localhost:3000/posts/${updatedPost.id}`, updatedPost)
+  return response.data
 }
 
 export async function deletePost(id) {
-  const response = await fetch(`http://localhost:3000/posts/${id}`, {
-    method: 'DELETE',
-  })
-  return response.json()
+  const response = await axios.delete(`http://localhost:3000/posts/${id}`)
+  return response.data
 }
+
+// export async function fetchPosts() {
+//   const response = await fetch('http://localhost:3000/posts')
+//   return response.json()
+// }
+
+// export async function fetchPost(id) {
+//   const response = await fetch(`http://localhost:3000/posts/${id}`)
+//   return response.json()
+// }
+
+// export async function createPost(newPost) {
+//   const response = await fetch(`http://localhost:3000/posts`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newPost),
+//   })
+//   return response.json()
+// }
+
+// export async function updatePost(updatedPost) {
+//   const response = await fetch(`http://localhost:3000/posts/${updatedPost.id}`, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(updatedPost),
+//   })
+//   return response.json()
+// }
+
+// export async function deletePost(id) {
+//   const response = await fetch(`http://localhost:3000/posts/${id}`, {
+//     method: 'DELETE',
+//   })
+//   return response.json()
+// }
